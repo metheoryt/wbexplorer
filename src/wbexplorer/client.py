d10db19445617d0209985df45f7dab7fe80ec7e0
Maxim Romanyuk <metheoryt@gmail.com>
2024-10-20 23:53:09 +0500

    fixes

--- src/wbexplorer/client.py
+++ src/wbexplorer/client.py
@@ -13,18 +13,21 @@ SEARCH_URL = 'https://search.wb.ru/exactmatch/sng/common/v7/search'
 
 
 class Client:
-    def __init__(self):
+    def __init__(self, destination: int = -1292286):
+        """
+        :param destination: delivery destination, defaults to some in Moscow.
+        """
+        self.dest = destination
         self.session = requests.Session()
         self.session.headers.update({'User-Agent': FakeUserAgent().getChrome['useragent']})
         # check connection, get any cookies required
         r = self.session.get(BASE_URL)
         r.raise_for_status()
 
-    async def search(self, query: str, dest: int) -> list[WBItem]:
+    def search(self, query: str) -> list[WBItem]:
         """
         Search for items.
         :param query: search query
-        :param dest: search destination, e.g. 123585479 for one in Moscow
         :return: list of WBItem objects.
         """
         response = self.session.get(
@@ -34,7 +37,7 @@ class Client:
                 'sort': 'popular',
                 'resultset': 'catalog',
                 'query': query,
-                'dest': dest,
+                'dest': self.dest,
                 'curr': 'rub',
                 'appType': 1,
                 'ab_testing': 'false',
@@ -46,17 +49,26 @@ class Client:
             try:
                 item = WBItem.from_dict(v)
             except Exception as e:
-                print(f'cannot parse into wb item, {e}:', json.dumps(v))
+                print(f'cannot parse into wb item, {type(e)} {e}:', json.dumps(v))
                 continue
             items.append(item)
         return items
 
-    async def price_history(self, item_id: int) -> list[tuple[datetime.date, Decimal]]:
-        vol = str(item_id)[:4]
-        part = str(item_id)[:6]
+    @staticmethod
+    def basket_vol_part(item_id: int) -> tuple[str, int, int]:
+        short_id = item_id // 100000
+        if short_id >= 10_000:
+            raise ValueError('short_id is too big')
+        # [)
+        baskets = [0, 144, 288, 432, 720, 1008, 1062, 1116, 1170, 1314, 1602, 1656, 1920, 2046, 2190, 10_000]
+        for i in range(1, len(baskets)):
+            if baskets[i - 1] <= short_id < baskets[i]:
+                return f'{i:02}', short_id, item_id // 1000
 
+    def price_history(self, item_id: int) -> list[tuple[datetime.date, Decimal]]:
+        basket, vol, part = self.basket_vol_part(item_id)
         response = self.session.get(
-            f'https://basket-{10}.wbbasket.ru/vol{vol}/part{part}/{item_id}/info/price-history.json'
+            f'https://basket-{basket}.wbbasket.ru/vol{vol}/part{part}/{item_id}/info/price-history.json'
         )
         response.raise_for_status()
         
